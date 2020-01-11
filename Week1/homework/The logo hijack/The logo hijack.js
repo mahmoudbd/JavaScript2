28fb368e6ed0a9ba8f647b383ed77f7c82a84fee
Mahmoud Badran <56569128+mahmoudbd@users.noreply.github.com>
2020-01-11 21:49:31 +0100

    Delete The logo hijack.js

--- Week1/homework/The logo hijack/The logo hijack.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const googleLogo = document.getElementById('hplogo');
-
-function hijackGoogleLogo() {
-	googleLogo.src = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
-	googleLogo.srcset = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
-}
-hijackGoogleLogo();
