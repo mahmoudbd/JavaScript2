0d0f4801cff1a24dcc578d8cef2d2957c9a063d1
Mahmoud Badran <56569128+mahmoudbd@users.noreply.github.com>
2020-01-11 21:49:45 +0100

    Delete showCurrentTime.js

--- Week1/homework/What's the time/showCurrentTime.js
+++
@@ -1,9 +0,0 @@
-function currentTime() {
-	const timeToShow = setInterval(timer, 1000);
-
-	function timer() {
-		const CT = new Date();
-		document.querySelector('#showLocalTime').innerHTML = CT.toLocaleTimeString();
-	}
-}
-currentTime();
