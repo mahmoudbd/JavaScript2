68875e445dd2c6fb2b04f30ad7c326644ad5d8b6
Mahmoud Badran <mahmoudbd@gmail.com>
2020-01-12 14:57:17 +0100

    deleted my home work

--- Week2/homework/Lemon allergy/lemonAllergy.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-const fruitBasket = [ 'Apple', 'Lemon', 'Grapefruit', 'Lemon', 'Banana', 'Watermelon', 'Lemon' ];
-
-const newFruitBasket = fruitBasket.filter((fruit) => !fruit.includes('Lemon'));
-console.log(`My mom bought me a fruit basket, containing ${newFruitBasket} !`);
